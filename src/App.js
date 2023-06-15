55c2069d95b7c72bc592ec28536366624d2f1f3d
Chandar Bhan <chandar@cwebconsultants.com>
2023-06-15 12:38:00 +0530

    commiting testBranch.jsx file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Calcu from './Calcu';
+import TestBranch from './TestBranch';
 function App() {
   return (
     <div className="App">
       <Calcu />
+      <TestBranch />
     </div>
   );
 }
---
+++ src/TestBranch.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TestBranch=()=>{
+    return(
+        <>
+        <>This is test branch</>
+        </>
+    )
+}
+
+export default TestBranch;
